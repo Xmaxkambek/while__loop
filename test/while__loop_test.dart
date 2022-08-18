e12c33f272567067b84ca76cdc57a350420a2960
sanjarbek <sanjarsaidov173@gamil.com>
2022-08-18 12:19:06 +0500

    beginning

---
+++ test/while__loop_test.dart
@@ -0,0 +1,122 @@
+import '../bin/while__loop.dart' as f;
+import 'package:test/test.dart';
+import 'dart:async';
+
+var log = [];
+
+var lst = [
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar',
+  'Anvar'
+];
+main() {
+  test('override print', overridePrint(() {
+    f.main();
+    expect(log, lst);
+  }));
+}
+
+void Function() overridePrint(void Function() testFn) => () {
+      var spec = ZoneSpecification(print: (_, __, ___, String msg) {
+        // Add to log instead of printing to stdout
+        log.add(msg);
+      });
+      return Zone.current.fork(specification: spec).run<void>(testFn);
+    };
