4044fa23124c5f9159b5dc0b243449c5cf88fb3d
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-30 08:54:49 +0500

    19

--- bin/while__loop19.dart
+++ bin/while__loop19.dart
@@ -1,3 +1,12 @@
+
+
 void main() {
+  int a = 0;
+  for(int b = 50;b < 1000; b++){
+    if(b%2==1){
+      a = a + b;
+  print(a);
+    }
+  }
   //Print the sum of odd numbers between 50 and 1000
 }
