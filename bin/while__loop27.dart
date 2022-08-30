6c0d25c61d788753fca08c4e5d7cbb439144d25e
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-30 11:19:53 +0500

    27

--- bin/while__loop27.dart
+++ bin/while__loop27.dart
@@ -1,3 +1,15 @@
 void main() {
-  //Print the first odd numbers and then the even numbers from 100 to 1000.
+ for (int a = 100; a < 1000; a++) {
+    if (a % 2 == 1) {
+      print(a);
+    }
+  }
+  for (int a = 100; a < 1000; a++) {
+    if (a % 2 == 0) {
+      print(a);
+    }
+  }
 }
+
+  //Print the first odd numbers and then the even numbers from 100 to 1000.
+
