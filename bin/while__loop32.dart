58bde3d439dc7d37c2cfd47e8af5a0800cc4b366
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-30 15:01:55 +0500

    32

--- bin/while__loop32.dart
+++ bin/while__loop32.dart
@@ -1,4 +1,17 @@
 void main() {
   //Add even numbers from 1 to 100 to the list.
   //Print the sum of the numbers in the even indices
+  List<int> m = [];
+  int n = 0;
+  for (int i = 1; i < 100; i++) {
+    if (i % 2 == 0) {
+      m.add(i);
+    }
+  }
+  for (int i = 0; i < m.length; i++) {
+    if (i % 2 == 0) {
+      n += m[i];
+    }
+  }
+  print(n);
 }
