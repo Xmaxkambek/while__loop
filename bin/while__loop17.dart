b5da2e3b9984e9f185bd481cc34f22d1a4e0e4f9
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-30 09:48:17 +0500

    17

--- bin/while__loop17.dart
+++ bin/while__loop17.dart
@@ -1,9 +1,9 @@
 void main() {
   int a = 0;
-  for(int x = 50;x < 1000;x++){
-    // a = a + x;
-    print(a);
+  for(int x = 50;x <= 1000;x++){
+    a = a + x;
 
   }
+    print(a);
   //Print the sum of the numbers from 50 to 1000
 }
