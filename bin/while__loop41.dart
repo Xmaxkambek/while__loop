2a3d6fbde6cb92c1cbac1d9021141867ec40b368
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-30 15:56:54 +0500

    41

--- bin/while__loop41.dart
+++ bin/while__loop41.dart
@@ -1,4 +1,17 @@
 void main() {
   //Add even numbers from 330 to 20 to the list.
   // Print the sum of the numbers at odd index.
+    List<int> m = [];
+  int n = 0;
+  for (int i = 330; i > 20; i--) {
+    if (i % 2 == 0) {
+      m.add(i);
+    }
+  }
+  for (int i = 0; i < m.length; i++) {
+    if (i % 2 == 1) {
+      n += m[i];
+    }
+  }
+  print(n);
 }
