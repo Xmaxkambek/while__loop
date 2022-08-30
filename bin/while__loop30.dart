d139860d80b9843f74a39d7f4c5d11077abc458f
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-30 11:36:12 +0500

    30

--- bin/while__loop30.dart
+++ bin/while__loop30.dart
@@ -1,3 +1,9 @@
 void main() {
-  //Add the numbers from 1 to 100 to the list. And print the list
+    List a = [];
+  for (int b = 1; b < 100; b++) {
+    a.add(b);
+  }
+  print(a);
 }
+  //Add the numbers from 1 to 100 to the list. And print the list
+
