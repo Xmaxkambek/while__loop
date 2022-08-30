11bef54c4a66cee1fec00de099739e06e68fb7eb
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-30 16:01:23 +0500

    43

--- bin/while__loop43.dart
+++ bin/while__loop43.dart
@@ -1,3 +1,10 @@
 void main() {
   //Add even numbers from 330 to 20 to the list.
+  List m = [];
+  for (int i = 330; i > 20; i--) {
+    if (i % 2 == 0) {
+      m.add(i);
+    }
+  }
+  print(m);
 }
